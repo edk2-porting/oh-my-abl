57183b92aa7a3a84b470321018f151f6ee0586c3
Melody Olvera <quic_molvera@quicinc.com>
2022-09-26 14:36:52 -0700

    QcomModulePkg: Add support for chip serial numbers
    Flashless devices cannot get the serial number of the device from the
    memory card, so add support for getting the serial thorugh the chip
    protocol.
    
    Change-Id: I40621709eb363dfedce83464d8740a1dc9425a0b

--- QcomModulePkg/Library/BootLib/Board.c
+++ QcomModulePkg/Library/BootLib/Board.c
@@ -27,9 +27,46 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "AutoGen.h"
 #include <Board.h>
 #include <Library/BootImage.h>
+#include <Library/UpdateCmdLine.h>
 #include <Library/UpdateDeviceTree.h>
 #include <Protocol/EFICardInfo.h>
 #include <Protocol/EFIPlatformInfoTypes.h>
@@ -583,6 +620,7 @@ BoardSerialNum (CHAR8 *StrSerialNum, UINT32 Len)
   EFI_STATUS Status = EFI_INVALID_PARAMETER;
   MEM_CARD_INFO CardInfoData;
   EFI_MEM_CARDINFO_PROTOCOL *CardInfo;
+  EFI_CHIPINFO_PROTOCOL *ChipInfo;
   UINT32 SerialNo;
   HandleInfo HandleInfoList[HANDLE_MAX_INFO_LIST];
   UINT32 MaxHandles = ARRAY_SIZE (HandleInfoList);
@@ -602,6 +640,23 @@ BoardSerialNum (CHAR8 *StrSerialNum, UINT32 Len)
                            &gEfiMemCardInfoProtocolGuid, (VOID **)&CardInfo);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Error locating MemCardInfoProtocol:%x\n", Status));
+
+    Status = gBS->LocateProtocol (&gEfiChipInfoProtocolGuid, NULL,
+                                  (VOID **)&ChipInfo);
+    if (Status != EFI_SUCCESS) {
+      DEBUG ((EFI_D_ERROR, "Error locating ChipInfoProtocol: %x\n", Status));
+      return Status;
+    }
+
+    Status = ChipInfo->GetSerialNumber (ChipInfo, &SerialNo);
+    if (Status != EFI_SUCCESS) {
+      DEBUG ((EFI_D_ERROR, "Error getting serial number from ChipInfo: %x\n",
+              Status));
+      return Status;
+    }
+
+    AsciiSPrint (StrSerialNum, Len, "%x", SerialNo);
+    ToLower (StrSerialNum);
     return Status;
   }
 
