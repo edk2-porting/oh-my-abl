5b29a0144f9c254467415580590ff396b621a12b
Shreyas K K <quic_shrekk@quicinc.com>
2023-03-27 16:15:23 +0530

    QcomModulePkg: Fix compiler error
    Define InitTaAndGetKey before its invocation to avoid
    compiler errors.
    
    Types like uintptr_t are defined in both avb_sysdeps.h
    and stdint.h. Avoid type redifinition by modifying the
    include headers.
    
    Change-Id: Idf9e1fc14552c25e2f8b62148b6dad69f0bfc742

--- QcomModulePkg/Library/aes/env_comdef.h
+++ QcomModulePkg/Library/aes/env_comdef.h
@@ -30,12 +30,50 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *************************************************************************/
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following
+ *  license:
+ *
+ *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights
+ *  reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /*===========================================================================
 
                      INCLUDE FILES FOR MODULE
 
 ===========================================================================*/
-#include <stdint.h>
+#include <avb/libavb/libavb.h>
 
 typedef enum
 {
@@ -208,17 +246,17 @@ typedef  int64_t    int64;       /* Signed 64 bit value */
 #endif
 
 #ifndef _INT32_DEFINED
-typedef  int32_t    int32;       /* Signed 32 bit value */
+typedef  INT32    int32;       /* Signed 32 bit value */
 #define _INT32_DEFINED
 #endif
 
 #ifndef _INT16_DEFINED
-typedef  int16_t       int16;       /* Signed 16 bit value */
+typedef  INT16       int16;       /* Signed 16 bit value */
 #define _INT16_DEFINED
 #endif
 
 #ifndef _INT8_DEFINED
-typedef  int8_t        int8;        /* Signed 8  bit value */
+typedef  INT8        int8;        /* Signed 8  bit value */
 #define _INT8_DEFINED
 #endif
 
