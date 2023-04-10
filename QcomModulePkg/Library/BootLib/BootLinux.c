edk2-porting/oh-my-abl

commit e92a93c3acf97e8a44823cdb31bfdc595759c4b6

QcomModulePkg: avb: Fix potential issues in edk2
Initialize variable to NULL and add
NULL check to avoid potential issues.

Change-Id: I963f06cd445eada2bf29bffadb406c6206ab2e65

QcomModulePkg/Library/BootLib/BootLinux.c
@@ -1207,7 +1207,7 @@ BootLinux (BootInfo *Info)
   CHAR16 *PartitionName = NULL;
   BOOLEAN Recovery = FALSE;
   BOOLEAN AlarmBoot = FALSE;
-  BOOLEAN FlashlessBoot = Info->FlashlessBoot;
+  BOOLEAN FlashlessBoot;
   CHAR8 SilentBootMode;
 
   LINUX_KERNEL LinuxKernel;
@@ -1226,14 +1226,16 @@ BootLinux (BootInfo *Info)
   UINTN DataSize;
   EFI_KERNEL_PROTOCOL *KernIntf = NULL;
   Thread *ThreadNum;
-  VOID *StackBase;
-  VOID **StackCurrent;
 #endif
+  VOID *StackBase = NULL;
+  VOID **StackCurrent = NULL;
 
   if (Info == NULL) {
     DEBUG ((EFI_D_ERROR, "BootLinux: invalid parameter Info\n"));
     return EFI_INVALID_PARAMETER;
   }
+
+  FlashlessBoot = Info->FlashlessBoot;
 
   if (IsVmEnabled ()) {
     Status = CheckAndSetVmData (&BootParamlistPtr);
@@ -1587,7 +1589,7 @@ CheckImageHeader (VOID *ImageHdrBuffer,
   boot_img_hdr_v3 *RecoveryImgHdrV3 = NULL;
   boot_img_hdr_v4 *BootImgHdrV4;
   vendor_boot_img_hdr_v4 *VendorBootImgHdrV4;
-  boot_img_hdr_v4 *RecoveryImgHdrV4;
+  boot_img_hdr_v4 *RecoveryImgHdrV4 = NULL;
 
   UINT32 KernelSizeActual = 0;
   UINT32 DtSizeActual = 0;

QcomModulePkg/Library/BootLib/DeviceInfo.c
@@ -29,7 +29,7 @@
 /*
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -523,8 +523,8 @@ EFI_STATUS WritePersistentValue (CONST UINT8 *Name, UINTN NameSize,
    EFI_STATUS Status = EFI_SUCCESS;
    BOOLEAN    NameFound = FALSE;
    BOOLEAN    SlotFound = FALSE;
-   UINT32     NameFoundIndex;
-   UINT32     SlotFoundIndex;
+   UINT32     NameFoundIndex = 0;
+   UINT32     SlotFoundIndex = 0;
    UINT32     i;
 
    if (FirstReadDevInfo) {

QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -1021,6 +1021,9 @@ AddtoBootConfigList (BOOLEAN BootConfigFlag,
   NewNode = (struct BootConfigParamNode *)
                AllocateBootConfigNode (ParamKeyLen + SIZE_OF_DELIM +
                SIZE_OF_DELIM + ParamValueLen);
+  if (!NewNode) {
+    return;
+  }
   gBS->CopyMem (NewNode->param, (CHAR8*)ParamKey, ParamKeyLen);
   if (ParamValue) {
     gBS->CopyMem (&NewNode->param[ParamKeyLen], (CHAR8*)ParamValue,

commit bacc7fff50cf9fad3724f05b4f418442b4014310

QcomModulePkg: Fix unused variable error
Fix for unused variable StackBase, StackCurrent error.

Fixes: e92a93c (QcomModulePkg: avb: Fix potential issues in edk2)
Change-Id: I10fbcdd16ec0d68cad9002edea6cea962524ba03

QcomModulePkg/Library/BootLib/BootLinux.c
@@ -1230,9 +1230,9 @@ BootLinux (BootInfo *Info)
   UINTN DataSize;
   EFI_KERNEL_PROTOCOL *KernIntf = NULL;
   Thread *ThreadNum;
-#endif
   VOID *StackBase = NULL;
   VOID **StackCurrent = NULL;
+#endif
 
   RamPartitionEntry *RamPartitions = NULL;
   UINT32 NumPartitions = 0;