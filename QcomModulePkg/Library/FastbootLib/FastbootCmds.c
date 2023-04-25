339a89161e701facdb5ce4a27e7366c6ad1f245c
Yanjie Zhang <quic_yanjzhan@quicinc.com>
2023-04-25 19:18:57 +0800

    Revert "avb: fix system will go to red"
    This reverts commit 3091dc36ac74696f1f7ab11c421de8c53d429cb0.
    Reverting commit as device is going into recovery mode after locking
    bootloader which isn't the desired result.
    
    Change-Id: Id9dee7a0dd77dcb5b06a14d5ce0370a4a33aae52

--- QcomModulePkg/Library/FastbootLib/FastbootCmds.c
+++ QcomModulePkg/Library/FastbootLib/FastbootCmds.c
@@ -3035,24 +3035,6 @@ is_display_supported ( VOID )
    return 1;
 }
 
-#ifndef TARGET_BOARD_TYPE_AUTO
-STATIC VOID
-RebootDeviceRecovery ( VOID )
-{
-   if (GetAVBVersion () != AVB_LE &&
-      !IsEnableDisplayMenuFlagSupported ()) {
-     RebootDevice (RECOVERY_MODE);
-   }
-
-}
-#else
-STATIC VOID
-RebootDeviceRecovery ( VOID )
-{
-
-}
-#endif
-
 STATIC VOID
 SetDeviceUnlock (UINT32 Type, BOOLEAN State)
 {
@@ -3096,7 +3078,10 @@ SetDeviceUnlock (UINT32 Type, BOOLEAN State)
          return;
     }
     FastbootOkay ("");
-    RebootDeviceRecovery ();
+    if (GetAVBVersion () != AVB_LE &&
+       !IsEnableDisplayMenuFlagSupported ()) {
+      RebootDevice (RECOVERY_MODE);
+    }
   }
 }
 #endif
